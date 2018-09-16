ee1b65a13cc84f1172696d491a5586acb77e4509
arefin <sultanul.arefin@nordecode.com>
2018-09-16 15:43:26 +0600

    navigation_views_awesomeProject_without_expo

--- App.js
+++ App.js
@@ -13,11 +13,15 @@ import {
 
 import { Container, Button, H3, Text } from "native-base";
 
+import SeptPage from "./src/components/SeptPage/SeptPage";
+
 // Code related to  GoogleAnalyticsBridge
 
 // You have access to three classes in this module:
 
 
+// import { createStackNavigator } from 'react-navigation';
+// Version can be specified in package.json
 
 
 //GoogleAnalyticsBridge
@@ -27,15 +31,19 @@ import { Container, Button, H3, Text } from "native-base";
 
 import styles from "./styles";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',react-native-device-info
+// });
 
 type Props = {};
-export default class App extends Component<Props> {
+
+
+
+
+export default class  extends Component<Props> {
     render() {
         return (
             /*
@@ -73,28 +81,36 @@ export default class App extends Component<Props> {
                         <View style={{marginTop: 8}}/>
                     </View>
                     <View style={{marginBottom: 80}}>
-                        <Button
-                            style={{backgroundColor: "#6FAF98", alignSelf: "center"}}
-                            onPress={() => this.props.navigation.navigate("DrawerOpen")}
-                            title="Will not be visible or rendered but added as required rule by arefin"
-                        >
-                            <Text>Lets Go!</Text>
-                        </Button>
-                        <Button
-                            style={{backgroundColor: "crimson", alignSelf: "center"}}
-                            onPress={() => this.props.navigation.navigate("Arefin")}
-                            title="Will not be visible or rendered but added as required rule by arefin"
-                        >
-                            <Text>Go To Arefin(custom)</Text>
-                        </Button>
+
+
+                        {/*<Button*/}
+                            {/*style={{backgroundColor: "#6FAF98", alignSelf: "center"}}*/}
+                            {/*onPress={() => this.props.navigation.navigate("DrawerOpen")}*/}
+                            {/*title="Will not be visible or rendered but added as required rule by arefin"*/}
+                        {/*>*/}
+                            {/*<Text>Lets Go!</Text>*/}
+                        {/*</Button>*/}
+
+
+                        {/*<Button*/}
+                            {/*style={{backgroundColor: "crimson", alignSelf: "center"}}*/}
+                            {/*onPress={() => this.props.navigation.navigate("Arefin")}*/}
+                            {/*title="Will not be visible or rendered but added as required rule by arefin"*/}
+                        {/*>*/}
+                            {/*<Text>Go To Arefin(custom)</Text>*/}
+                        {/*</Button>*/}
 
                         <Button
                             style={{backgroundColor: "#6FAF98", alignSelf: "center", marginBottom: 10}}
-                            onPress={() => this.props.navigation.navigate("ArefinWebView")}
+                            onPress={() => this.props.navigation.navigate("SeptPage")}
                             title="Will not be visible or rendered but added as required rule by arefin" sy
                         >
                             <Text>Arefin WebView</Text>
                         </Button>
+
+
+
+
 
                         <Button
                             /* onPress={() => console.log('2nd')}*/
@@ -131,29 +147,29 @@ let _cat2 = () => {
     // console.log("DeviceInfo.name: ",DeviceInfo.name);
     // console.log("DeviceInfo.model: ",DeviceInfo.model());
 
-    let DeviceInfo = require('react-native-device-info');
+    // let DeviceInfo = require('react-native-device-info');
 
-    console.log("DeviceInfo.getModel: ",DeviceInfo.getModel());
-
-    console.log("getDeviceName(): ",DeviceInfo.getDeviceName());
-
-
-    console.log("getUniqueID(): ",DeviceInfo.getUniqueID());
-    console.log("isEmulator(): ",DeviceInfo.isEmulator());
-
-    console.log("Window Dimensions is: ",Dimensions.get("window"));
-
-    console.log("Screen Dimensions is: ",Dimensions.get("screen"));
-
-
-
-    console.log("a: ",122211);
-    let OS_app;
-
-    if(Platform.OS === 'ios'){
-        OS_app ="ios";
-    }
-    else OS_app ="Android";
+    // console.log("DeviceInfo.getModel: ",DeviceInfo.getModel());
+    //
+    // console.log("getDeviceName(): ",DeviceInfo.getDeviceName());
+    //
+    //
+    // console.log("getUniqueID(): ",DeviceInfo.getUniqueID());
+    // console.log("isEmulator(): ",DeviceInfo.isEmulator());
+    //
+    // console.log("Window Dimensions is: ",Dimensions.get("window"));
+    //
+    // console.log("Screen Dimensions is: ",Dimensions.get("screen"));
+    //
+    //
+    //
+    // console.log("a: ",122211);
+    // let OS_app;
+    //
+    // if(Platform.OS === 'ios'){
+    //     OS_app ="ios";
+    // }
+    // else OS_app ="Android";
 
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
+import SetPage from './src/components/SeptPage/SeptPage';
 import App from './App';
 
-AppRegistry.registerComponent('AwesomeProject', () => App);
+// AppRegistry.registerComponent('AwesomeProject', () => App);
+
+AppRegistry.registerComponent('AwesomeProject', () => SetPage);
